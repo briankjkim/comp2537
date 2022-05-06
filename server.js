31515c019963df66e8273a664c04a00af235ebed
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-06 13:06:49 -0700

    implement basic login feature with express session

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+var session = require('express-session')
 
-// => is anonymous function; same as function(err)
-// this is to display error message
-app.listen(5000, (err) => {
+// Use the session middleware
+app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: true }));
+
+users = {
+    "user1": "pass1",
+    "user2": "pass2",
+}
+
+
+app.listen(5000, function (err) {
     if (err) console.log(err);
 })
 
 app.get('/', function (req, res) {
-    res.send('GET request to homepage')
+    if (req.session.authenticated)
+        res.send(`Hi ${req.session.user} !`)
+    else {
+        res.redirect('/login')
+    }
 })
 
-app.get('/profile/:id', function (req, res) {
-    // console.log(req);
-    res.render("profile.ejs", {
-        "id": req.params.id
-    });
+
+app.get('/login/', function (req, res, next) {
+    res.send("Please provide the credentials through the URL")
+})
+
+app.get('/login/:user/:pass', function (req, res, next) {
+    if (users[req.params.user] == req.params.pass) {
+        req.session.authenticated = true
+        req.session.user = req.params.user
+        res.send("Successful Login!")
+    } else {
+        req.session.authenticated = false
+        res.send("Failed Login!")
+    }
+
 })
