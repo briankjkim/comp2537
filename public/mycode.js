bfc92e63f17a08b060382d88bde1229bb04b4b17
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-08 14:53:37 -0700

    implement asyncrous function to display 3 x 3 random pokemon images in index

--- public/mycode.js
+++ public/mycode.js
@@ -1,25 +1,45 @@
-pokemonGridImages = ""
+pokemonGridImages = ''
 
-function loadPokemonImages() {
+function processPokeResp(data) {
+    console.log(data.id)
+    pokemonGridImages += ` ${data.name}
+      <div class="image_container">
+      <a href="/profile/${data.id}">  
+      <img src="${data.sprites.other["official-artwork"].front_default}">
+      </a>
+      </div>`
+}
+
+async function loadPokemonImages() {
     for (i = 1; i <= 9; i++) {
         if (i % 3 == 1) { // only when i = 1, 4, 7
             pokemonGridImages += `<div class="images_group">`
         }
 
-        pokemonGridImages += `<div class="image_container"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/60.png"></div>`
+        // 1- generate a random number for Pokemon ID so that images can be loaded
+        pokemonRandomID = Math.floor(Math.random() * 150) + 1
+
+        // 2- initialize an AJAX request to pokeapi.co
+        await $.ajax({
+            type: "GET",
+            url: `https://pokeapi.co/api/v2/pokemon/${pokemonRandomID}/`,
+            success: processPokeResp
+        })
 
         if (i % 3 == 0) { // only when i = 3, 6, 9
             pokemonGridImages += `</div>`
         }
-    }    
+    }
     $("main").html(pokemonGridImages)
-
 }
+
+
+
+// `<div class="image_container"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png"></div>`
 
 function setup() {
     loadPokemonImages();
     // event handlers come here
 }
-
 
 $(document).ready(setup)
