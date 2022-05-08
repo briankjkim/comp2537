96f092fc582b19847de634567f6c7cfa763836a1
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-08 13:49:07 -0700

    implement function to load pokemon images to index

--- public/mycode.js
+++ public/mycode.js
@@ -1,10 +1,24 @@
+pokemonGridImages = ""
 
+function loadPokemonImages() {
+    for (i = 1; i <= 9; i++) {
+        if (i % 3 == 1) { // only when i = 1, 4, 7
+            pokemonGridImages += `<div class="images_group">`
+        }
 
+        pokemonGridImages += `<div class="image_container"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/60.png"></div>`
 
+        if (i % 3 == 0) { // only when i = 3, 6, 9
+            pokemonGridImages += `</div>`
+        }
+    }    
+    $("main").html(pokemonGridImages)
+
+}
 
 function setup() {
-    console.log("document is ready")
-
+    loadPokemonImages();
+    // event handlers come here
 }
 
 
