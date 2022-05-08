4d9dea386795c662e0d175d283dcf07b703f8b4f
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-08 12:48:27 -0700

    fix req,res typo in get profile route

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express')
 const app = express()
 
-
 app.set('view engine', 'ejs');  
-
 
 app.listen(5000, function (err) {
     if (err) console.log(err);
 })
 
-
-app.get('/profile/:id', function(res, req){
+app.get('/profile/:id', function(req, res){
     res.render("profile.ejs", {
         "id": req.params.id
     });
