67dd04ae02e56e6219dcfa64a6a3cfb0e6fb7bb3
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-08 12:29:38 -0700

    remove login/logout session from server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-var session = require('express-session');
 
 app.set('view engine', 'ejs');  
 
@@ -15,43 +14,6 @@ app.get('/profile/:id', function(res, req){
     res.render("profile.ejs", {
         "id": req.params.id
     });
-})
-
-
-// Use the session middleware for basic login/logout feature
-app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: true }));
-
-users = {
-    "user1": "pass1",
-    "user2": "pass2",
-}
-
-
-
-
-app.get('/', function (req, res) {
-    if (req.session.authenticated)
-        res.send(`Hi ${req.session.user} !`)
-    else {
-        res.redirect('/login')
-    }
-})
-
-
-app.get('/login/', function (req, res, next) {
-    res.send("Please provide the credentials through the URL")
-})
-
-app.get('/login/:user/:pass', function (req, res, next) {
-    if (users[req.params.user] == req.params.pass) {
-        req.session.authenticated = true
-        req.session.user = req.params.user
-        res.send("Successful Login!")
-    } else {
-        req.session.authenticated = false
-        res.send("Failed Login!")
-    }
-
 })
 
 app.use(express.static('./public'));
