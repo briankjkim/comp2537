5f1f6429c731e76a7c98f9977a67409c1099e95d
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-08 17:34:13 -0700

    fix search page and search.js

--- public/search.js
+++ public/search.js
@@ -1,30 +1,38 @@
 type_global = ""
 
-function processPokemonResp(data){
-    for (i = 0; i < data.types.length; i++)
-        if(data.types[i].type.name == type_global)
-            $("main").html(data.id)
+function processPokemonResp(data) {
+    // for each pokemon, check if filtered type matches the actual type
+    for (i = 0; i < data.types.length; i++) {
+        if (data.types[i].type.name == type_global)
+            $("main").append("<p>"+data.id+"</p>")
+    }
 }
 
-function display(type_){
-    for(i = 1; i <= 151; i++){
+
+function display(type_) {
+    $("main").empty()
+    type_global = type_
+    // iterate over first gen pokemons
+    for (i = 1; i <= 151; i++) {
         $.ajax({
             type: "get",
-            URL: `https://pokeapi.co/api/v2/pokemon/${i}`,
+            url: `https://pokeapi.co/api/v2/pokemon/${i}`,
             success: processPokemonResp
         })
     }
-    $("main").html()
+
 }
 
 
 function setup() {
-
+    display($("#poke_type option:selected").val())
 
     $("#poke_type").change(() => {
         poke_type = $("#poke_type option:selected").val();
         display($("#poke_type option:selected").val())
+
     })
 }
+
 
 $(document).ready(setup)
