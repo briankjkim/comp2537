4dacb0ca057b1eb3215480f23d6c294cf801c3ba
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-08 21:45:42 -0700

    add search by habitat for search page

--- public/search.js
+++ public/search.js
@@ -1,29 +1,49 @@
-function processPokemonResp(data) {
-    // for each pokemon, check if filtered type matches the actual type
+function processPokemonRespByType(data) {
     
     for (i = 0; i < data.pokemon.length; i++){
-        $("main").append("<p>"+data.pokemon[i].pokemon.name+"</p>")
+        $("main").append("<div>"+data.pokemon[i].pokemon.name+"</div>")
+    }
+
+}
+
+function processPokemonRespByHabitat(data) {
+    console.log(data)
+    for (i = 0; i < data.pokemon_species.length; i++){
+        $("main").append("<div>"+data.pokemon_species[i].name+"</div>")
     }
 
 }
 
 
-function display(type_num) {
+function display_by_type(type_num) {
     $("main").empty()
     $.ajax({
         type: "get",
         url: `https://pokeapi.co/api/v2/type/${type_num}/`,
-        success: processPokemonResp
+        success: processPokemonRespByType
+    })
+}
+
+function display_by_habitat(type_num) {
+    $("main").empty()
+    $.ajax({
+        type: "get",
+        url: `https://pokeapi.co/api/v2/pokemon-habitat/${type_num}/`,
+        success: processPokemonRespByHabitat
     })
 }
 
 
-function setup() {
-    display($("#poke_type option:selected").val())
 
+function setup() {
     $("#poke_type").change(() => {
         poke_type = $("#poke_type option:selected").val();
-        display($("#poke_type option:selected").val())
+        display_by_type($("#poke_type option:selected").val())
+    })
+
+    $("#poke_habitat").change(() => {
+        poke_habitat = $("#poke_habitat option:selected").val();
+        display_by_habitat($("#poke_habitat option:selected").val())
     })
 }
 
