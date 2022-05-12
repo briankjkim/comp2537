7b465941b0aae1acd7ce3e7c129db85fe2ebee68
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-12 15:50:52 -0700

    add four CRUD routes to implement timeline component and connect with mongoose, mongodb

--- server.js
+++ server.js
@@ -10,13 +10,29 @@ app.use(cors());
 
 
 // Connect to mongodb with mongoose module
-mongoose.connect("mongodb://localhost:27017/test",
- {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Create eventScheme for timeline events
+const eventSchema = new mongoose.Schema({
+    text: String,
+    hits: Number,
+    time: String
+});
+
+// Create eventModel for mongoose module
+const eventModel = mongoose.model("timelineevents", eventSchema);
 
 
 app.use(bodyparser.urlencoded({
+    parameterLimit: 100000,
+    limit: '50mb',
     extended: true
-  }));
+}));
+
+
 
 app.set('view engine', 'ejs');
 
@@ -26,9 +42,75 @@ app.listen(process.env.PORT || 5000, function (err) {
 })
 
 
+// READ timeline data from the server with GET Request
+app.get('/timeline/getAllEvents', function (req, res) {
+    console.log("Received a GET request for timeline database");
+    eventModel.find({}, function (err, data) {
+        if (err) {
+            console.log("Error " + err);
+        } else {
+            console.log("Data " + data);
+        }
+        res.send(data);
+    });
+})
+
+
+// CREATE a timeline event to the data with PUT Request
+app.put('/timeline/insert', function (req, res) {
+    console.log(req.body)
+    console.log("Received a PUT request to insert a timeline event to database");
+    eventModel.create({
+        text: req.body.text,
+        time: req.body.time,
+        hits: req.body.hits
+    }, function (err, data) {
+        if (err) {
+            console.log("Error " + err);
+        } else {
+            console.log("Data " + data);
+        }
+        res.send(data);
+    });
+})
+
+
+// Update a timeline event by incrementing hits
+app.get('/timeline/inreaseHits/:id', function (req, res) {
+    console.log(req.params)
+    eventModel.updateOne({
+       _id : req.params.id
+    }, {
+        $inc : { hits: 1}
+    }, function (err, data) {
+        if (err) {
+            console.log("Error " + err);
+        } else {
+            console.log("Data " + data);
+        }
+        res.send("Event is updated.");
+    });
+})
+
+
+// Delete a timeline event from the database with GET Request
+app.get('/timeline/remove/:id', function (req, res) {
+    // console.log(req.params)
+    eventModel.remove({
+       _id : req.params.id
+    }, function (err, data) {
+        if (err) {
+            console.log("Error " + err);
+        } else {
+            console.log("Data " + data);
+        }
+        res.send("Event is deleted");
+    });
+})
+
+
+// GET Request route for Pokemon Profile page
 app.get('/profile/:id', function (req, res) {
-
-
     poke_id = req.params.id;
     const url = `https://cryptic-wildwood-03560.herokuapp.com/${poke_id}`
 
@@ -40,7 +122,7 @@ app.get('/profile/:id', function (req, res) {
         })
 
         https_res.on("end", function () {
-            data = JSON.parse(data)        
+            data = JSON.parse(data)
 
             res.render("profile.ejs", {
                 "id": data.id,
@@ -56,4 +138,6 @@ app.get('/profile/:id', function (req, res) {
     })
 });
 
+
+// Route for displaying static pages in public folder
 app.use(express.static('./public'));
