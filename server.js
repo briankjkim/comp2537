cbf65cf7c967dae6c1b0afc5db586429689b33bd
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-22 21:28:11 -0700

    remove obsolte codes

--- server.js
+++ server.js
@@ -19,19 +19,10 @@ app.use(bodyparser.json());
 app.use(cookieParser());
 
 const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
 const session = require('express-session')
-const methodOverride = require('method-override')
 
-const initializePassport = require('./passport-config')
-initializePassport(
-  passport,
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
-)
 
-const users = []
+
 
 // use cross origin cors
 app.use(
@@ -62,22 +53,6 @@ app.get('/', function (req, res) {
         res.render('login.ejs');
     }
 })
-
-
-
-// Passport codes
-app.use(express.urlencoded({ extended: false }))
-app.use(flash())
-app.use(session({
-  secret: 'hello',
-  resave: true,
-  saveUninitialized: true
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
-
-
 
 // Mongodb address "mongodb+srv://kkjin0330:5VO1M61v9prYQEpp@cluster0.msyad.mongodb.net/hello"
 
